105aa0fdbba0c50199d3ec20c25c4f83bd00925d
Yordan Ivanov <mail@ivanovyordan.com>
2021-03-23 09:02:20 +0200

    Add ability to install prod dependencies

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,6 +2,11 @@ import os
 
 os.system("poetry install")
 os.system("poetry add --dev {{ cookiecutter.dev_dependencies }}")
+
+
+if "{{ cookiecutter.dependencies }}" != "":
+    os.system("poetry add {{ cookiecutter.dependencies }}")
+
 os.system("git init .")
 os.system("poetry run pre-commit install")
 os.system("git add .")
